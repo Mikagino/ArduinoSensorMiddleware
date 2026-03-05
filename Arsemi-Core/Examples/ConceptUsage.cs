11be03c967354a8af449fd41fd283f768379084b
Mikagino <angeli.mika@gmail.com>
2026-03-05 19:00:13 +0900

    test: using Arsemi-Core inside godot

--- Arsemi-Core/Examples/ConceptUsage.cs
+++ Arsemi-Core/Examples/ConceptUsage.cs
@@ -1,5 +1,3 @@
-using System.Threading.Tasks;
-
 namespace Arsemi {
     namespace Examples {
         /// <summary>
--- arsemi-godot-plugin/Arsemigo.cs
+++ arsemi-godot-plugin/Arsemigo.cs
@@ -1,10 +1,28 @@
+using System.Threading;
+using Arsemi;
 using Godot;
 
-public partial class Arsemigo : Node
-{
-    private MemoryMappedSensorData memoryMappedSensorData = new();
+public partial class Arsemigo : Node {
+    [Export] public Label DebugLabel;
 
-    public override void _Process(double delta)
-    {
+    private ArsemiCore _asmCore = new();
+
+    public override void _Ready() {
+        _asmCore.StartSetup();
+        _asmCore.AddSensor(new Arsemi.Sensor.AnalogSensor(), "Heartrate")
+            .AddFilter(new Arsemi.Sensor.Filter.BlaBlaFilter())
+            .SetInterval(100)
+            .AddEvent(Arsemi.Sensor.AbstractSensor.EventType.Threshold, "Excitement", 120);
+        _asmCore.AddSensor(new Arsemi.Sensor.DigitalSensor(), "Button");
+        _asmCore.SetInterval("Button", 200);
+        _asmCore.FinishSetup();
+        // ExampleConstants.Events.Excitement += EventAction;
+
+        // ArsemiConfig.SaveTo(_asmCore, PathToConfigFile, ArsemiConfig.ConfigType.JSON);
+        _asmCore.StartLoop();
+    }
+
+    public override void _Process(double delta) {
+        DebugLabel.Text = _asmCore.GetSensorValue((uint)Arsemi.Examples.ExampleConstants.Sensors.Heartrate).ToString();
     }
 }
