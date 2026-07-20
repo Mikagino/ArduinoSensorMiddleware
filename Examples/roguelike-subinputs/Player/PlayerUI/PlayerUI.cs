484932e9c094f3f3de274906e53cfffdf4f5f40f
Mikagino <angeli.mika@gmail.com>
2026-07-20 13:55:06 +0900

    feat: out of screen waypoint (not working yet)

--- Examples/roguelike-subinputs/Player/PlayerUI/PlayerUI.cs
+++ Examples/roguelike-subinputs/Player/PlayerUI/PlayerUI.cs
@@ -1,50 +1,64 @@
-using System;
 using Godot;
+using Weapon;
 
-public partial class PlayerUI : Control {
-    private ProgressBar _healthbar;
-    private Label _ammunitionLabel;
-    private Control _gameOver;
-
-
-    public override void _Ready() {
-        _healthbar = GetNode<ProgressBar>("%HealthBar");
-        _ammunitionLabel = GetNode<Label>("%AmmunitionLabel");
-        _gameOver = GetNode<Control>("%GameOver");
-    }
+namespace Player {
+    public partial class PlayerUI : Control {
+        private ProgressBar _healthbar;
+        private Label _ammunitionLabel;
+        private Control _gameOver;
+        private OutOfScreenIcon _outOfScreenIcon;
 
 
-    public void SetHealth(int newHealth) {
-        _healthbar.Value = newHealth;
-    }
+        public override void _Ready() {
+            _healthbar = GetNode<ProgressBar>("%HealthBar");
+            _ammunitionLabel = GetNode<Label>("%AmmunitionLabel");
+            _gameOver = GetNode<Control>("%GameOver");
+            _outOfScreenIcon = GetNode<OutOfScreenIcon>("%OutOfScreenIcon");
+        }
 
 
-    public void SetAmmunition(int newAmmunition) {
-        if(_ammunitionLabel == null) return;
-        _ammunitionLabel.Text = newAmmunition.ToString();
-        _ammunitionLabel.SelfModulate = Color.Color8(255, 255, 255, 255);
-    }
+        public void SetHealth(int newHealth) {
+            _healthbar.Value = newHealth;
+        }
 
 
-    public void AmmunitionEmptied() {
-        _ammunitionLabel.Text = "X";
-        _ammunitionLabel.SelfModulate = Color.Color8(255, 0, 0, 255);
-    }
+        public void SetAmmunition(int newAmmunition) {
+            if(_ammunitionLabel == null) return;
+            _ammunitionLabel.Text = newAmmunition.ToString();
+            _ammunitionLabel.SelfModulate = Color.Color8(255, 255, 255, 255);
+        }
 
 
-    public void GameOver() {
-        GetTree().Paused = true;
-        _gameOver.Show();
-    }
+        public void AmmunitionEmptied() {
+            _ammunitionLabel.Text = "X";
+            _ammunitionLabel.SelfModulate = Color.Color8(255, 0, 0, 255);
+        }
 
 
-    public void ReloadCurrentScene() {
-        GetTree().ReloadCurrentScene();
-        GetTree().Paused = false;
-    }
+        // public void SetWeaponOutOfScreen(Area2D weapon) {
+        //     if(weapon.GetParent() is not WeaponItem) {
+        //         GD.Print("Out of screen not weapon");
+        //         return;
+        //     }
+
+        //     _outOfScreenIcon.Initialize(weapon.GetParent<WeaponItem>());
+        // }
 
 
-    public void ExitGame() {
-        GetTree().Quit();
+        public void GameOver() {
+            GetTree().Paused = true;
+            _gameOver.Show();
+        }
+
+
+        public void ReloadCurrentScene() {
+            GetTree().Paused = false;
+            GetTree().ReloadCurrentScene();
+        }
+
+
+        public void ExitGame() {
+            GetTree().Quit();
+        }
     }
 }
